b99e8c94e69b13da3c41c60b7430f0696d77f144
woodenonesie <olga.pi4ugina@yandex.ru>
2023-01-18 01:05:03 -0800

    new survey

---
+++ survey.js
@@ -0,0 +1,33 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What\'s your name? Nicknames are also acceptable :)', (name) => {
+  rl.question('What\'s an activity you like doing?', (activity) => {
+    rl.question('What do you listen to while doing that?', (music) => {
+      rl.question('Which meal is your favourite (eg: dinner, brunch, etc.)', (meal) => {
+        rl.question('What\'s your favourite thing to eat for that meal?', (favFood) => {
+          rl.question('Which sport is your absolute favourite?', (sport) => {
+            rl.question('What is your superpower? In a few words, tell us what you are amazing at!', (superpower) => {
+              console.log(`${name} loves listening to ${music} while ${activity}, devouring ${favFood} for ${meal}, prefers ${sport} over any other sport, and is amazing at ${superpower}.`);
+
+              rl.close();
+            })
+          })
+        })
+      })
+    })
+  })
+});
+
+
+// What's your name? Nicknames are also acceptable :)
+// What's an activity you like doing?
+// What do you listen to while doing that?
+// Which meal is your favourite (eg: dinner, brunch, etc.)
+// What's your favourite thing to eat for that meal?
+// Which sport is your absolute favourite?
+// What is your superpower? In a few words, tell us what you are amazing at!
